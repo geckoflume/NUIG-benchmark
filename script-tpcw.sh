2425a384aecf8bdf7a392f4b0e4609bbd0ec97c5
geckoflume <geckoflume@tuta.io>
2018-05-10 11:49:55 +0100

    Updated TPC-W script
    Added option to plot the results, fixed result analysis output and script now stops database server on install complete

--- script-tpcw.sh
+++ script-tpcw.sh
@@ -3,8 +3,8 @@
 #Credits to José Pereira from University of Minho (https://github.com/jopereira/java-tpcw.git)
 #Result content will be in tpcwResult.txt
 
-if [[ $# -ne 1 ]] || [[ "$1" != "install" && "$1" != "run" ]]; then
-	echo Syntax : $0 install or $0 run
+if [[ $# -ne 1 ]] || [[ "$1" != "install" && "$1" != "run" && "$1" != "plot" ]]; then
+	echo Syntax : $0 install or $0 run or $0 plot
 	exit 1
 fi
 case $1 in
@@ -23,7 +23,7 @@ case $1 in
 		#Creating new database, with user=admin and password=admin
 		java -jar mckoidb.jar -create "admin" "admin"
 		#Launching mckoidb server
-		java -jar mckoidb.jar &
+		java -jar mckoidb.jar & PID=$!
 		cd ~/TPCW
 		#Downloading java-tpcw
 		git clone https://github.com/jopereira/java-tpcw.git
@@ -34,6 +34,7 @@ case $1 in
 		echo "webappDir=/var/lib/tomcat7/webapps/" >> main.properties
 		#Installing java-tpcw
 		sudo ant inst
+		sleep 10
 		sudo ant genimg
 		echo "Database setup..."
 		#Filling database
@@ -41,14 +42,24 @@ case $1 in
 		#Downloading result analysis script
 		wget https://gist.githubusercontent.com/jopereira/4086237/raw/9b279cab3c8565b707808e299fe1811f1540d625/showtpc.py
 		chmod u+x showtpc.py
+		kill -2 $PID
 		exit 0
 		;;
 	run)
 		echo "Running TCP-W benchmark... "
+		#Launching mckoidb server
+		java -jar ~/TPCW/mckoi1.0.6/mckoidb.jar & PID=$!
+		cd ~/TPCW/java-tpcw
 		#Running Remote Browser Emulator (TPC-W benchmark)
 		./rbe.sh
 		#Running result analysis script
-		./showtpc.py --bench=w -p run1_* > tpcwResult.txt
+		./showtpc.py --bench=w $(ls run1_* | tail -1) > tpcwResult.txt
 		echo "Result located in tpcwResult.txt"
+		exit 0
+		;;
+	plot)
+		echo "Plotting result... "
+		#Running result analysis script
+		./showtpc.py --bench=w -p $(ls run1_* | tail -1)
 		exit 0
 esac
